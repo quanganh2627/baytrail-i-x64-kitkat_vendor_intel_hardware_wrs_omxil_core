67733d0a22800516bfda1cf3763027fbdddd7031
gji2 <guoliang.ji@intel.com>
2014-06-11 04:24:03 +0800

    resolution limit setting, max 1920x 1088
    BZ: 203491
    
    resolution limit setting, max 1920x 1088
    
    Change-Id: Ia155509fd76a6f77f25396f52baa6502641cd5d6
    Signed-off-by: gji2 <guoliang.ji@intel.com>

--- base/src/componentbase.cpp
+++ base/src/componentbase.cpp
@@ -585,6 +585,9 @@ OMX_ERRORTYPE ComponentBase::CBaseSetParameter(
         }
 
         if (working_role != NULL && !strncmp((char*)working_role, "video_encoder", 13)) {
+            if (p->format.video.nFrameWidth > 2048 || p->format.video.nFrameHeight > 2048)
+                return OMX_ErrorUnsupportedSetting;
+
             if(p->format.video.eColorFormat == OMX_COLOR_FormatUnused)
                 p->nBufferSize = p->format.video.nFrameWidth * p->format.video.nFrameHeight *3/2;
         }
